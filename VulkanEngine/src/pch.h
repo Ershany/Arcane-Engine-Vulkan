43d5f51c6d70a51db6c3a48e4cfc765be0642218
Ershany <bradyjessup@hotmail.com>
2020-02-29 13:13:08 -0500

    Initial Project Setup

---
+++ VulkanEngine/src/pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+
+#include <cmath>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <deque>
+#include <unordered_map>
+#include <array>
+#include <set>
+#include <iterator>
+#include <fstream>
+#include <random>
+
+#include <vulkan/vulkan.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/common.hpp>
+#include <glm/gtx/norm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+

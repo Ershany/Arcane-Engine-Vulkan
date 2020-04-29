8953aa36ac2f21c1a336b5ad8bcffd375a518d0c
Ershany <bradyjessup@hotmail.com>
2020-04-29 18:44:33 -0400

    Added asserts

--- Arcane/src/Core/Core.h
+++ Arcane/src/Core/Core.h
@@ -9,3 +9,12 @@
 #else
 	#error Arcane only supports Windows
 #endif
+
+// TODO: Just log instead of not in debug
+#ifdef ARC_DEBUG
+#define ARC_ENGINE_ASSERT(x, ...) { if(!(x)) { ARC_ENGINE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define ARC_GAME_ASSERT(x, ...) { if(!(x)) { ARC_GAME_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define ARC_ENGINE_ASSERT(x, ...)
+#define ARC_GAME_ASSERT(x, ...)
+#endif
--- Arcane/src/Core/Window.cpp
+++ Arcane/src/Core/Window.cpp
@@ -1,6 +1,8 @@
 #include "arcpch.h"
 #include "Window.h"
 
+#include "Core/Logger.h"
+
 namespace Arcane
 {
 	Window::Window(const char *title, int width, int height) : m_Title(title), m_Width(width), m_Height(height)
@@ -22,6 +24,7 @@ namespace Arcane
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+		ARC_ENGINE_INFO("Creating window ({0}, {1})", m_Width, m_Height);
 		m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, nullptr, nullptr);
 	}
 
